7fa933dafc8cdbb3e937f4f816b3d76a5ea52d05
Sirje Hanski <sirje.m.hanski@utu.fi>
2019-10-21 19:17:00 +0300

    TagList

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import {bindActionCreators, dispatch} from 'redux';
-import {connect} from 'react-redux';
-import * as actionCreators from '../actions/actionCreators'
+import React, { Component } from "react";
+import { bindActionCreators, dispatch } from "redux";
+import { connect } from "react-redux";
+import * as actionCreators from "../actions/actionCreators";
+
+const createTagList = project => <p>jotain </p>;
 
 const Project = props => {
-    const project = props.project
-    return (
-         <article className = "projects__article project">
-            <div><h3 className = "project__title">{project.title}</h3></div>
-            <div>{project.description}</div>
-            </article>
-      );
-} 
-  
+  const project = props.project;
+  return (
+    <article className="projects__article project">
+      <h3 className="project__title">{project.title}</h3>
+      <div>{project.description}</div>
+      {createTagList(project)}
+    </article>
+  );
+};
 
 export default Project;
